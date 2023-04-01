1035cbd2fe8eb751df0acac03b776559f8bfc74b
Ralph Harris <33107418+rahaprogramming@users.noreply.github.com>
2023-04-01 12:05:34 -0500

    Add files via upload
    minor change to add events to calendar

--- index.php
+++ index.php
@@ -6,13 +6,17 @@
 /** Loads the Semi-Configured Environment */
 require __DIR__ . '/config.php';
 if(!isset($database)){$database = new Database();}//initialize database class
-
-$database->renderPage(__DIR__  . '/header.php');
-
 if(strlen(page) > 1){
-    $database->renderPage(__DIR__  .'/pages'.page.'.php');
-}else if(page === "/"){
+    $database->renderPage(__DIR__  . '/header.php');
+    if(page === '/calendar'){//render calendar page
+        $database->renderPage(__DIR__  .'/pages'.page.'.php');
+        $database->renderPage(__DIR__  .'/pages/sections/addEvent.php');
+    }else{//render any page in the navigation
+        $database->renderPage(__DIR__  .'/pages'.page.'.php');
+    }
+    $database->renderPage(__DIR__  .'/footer.php');
+}else if(page === "/"){//render the home page
+    $database->renderPage(__DIR__  . '/header.php');
     $database->renderPage(__DIR__  .'/pages/home.php');
-}
-
-$database->renderPage(__DIR__  .'/footer.php');
+    $database->renderPage(__DIR__  .'/footer.php');
+} 
