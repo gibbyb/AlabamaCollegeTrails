5e826329015bb02028a48b57735872806889d942
Ralph Harris <33107418+rahaprogramming@users.noreply.github.com>
2023-04-05 15:59:32 -0500

    minor changes

--- pages/calendar.php
+++ pages/calendar.php
@@ -14,7 +14,6 @@ $events = $database->getEvents();
                 <h1 data-aos="fade-up" class="h1 aos-init aos-animate text-white ">Upcoming Events<span class="cursive">.</span></h1>
                 <h2 data-aos="fade-up" class="h2 aos-init aos-animate text-white ">Hiking<span class="cursive">,</span> School<span class="cursive">,</span> or Community Events
                     <span class="cursive">.</span></h2>
-                
             </div>
         </div>
     </div>
