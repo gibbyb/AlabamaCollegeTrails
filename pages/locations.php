7364e719ffc3c4bb84da0f304df608973f83e61d
Christian Patton <christiandpatton@gmail.com>
2023-04-14 18:40:20 -0500

    Reverted to previous design for locations page.

--- pages/locations.php
+++ pages/locations.php
@@ -1,36 +1,24 @@
 <?php 
   $trails_file = file_get_contents('./js/locations.json');
   $trails = json_decode($trails_file, true);
-  //see https://getbootstrap.com/docs/5.0/layout/grid/ for bootstrap grid breakdown. It's easy once you know it...
 ?>
-<section class="mt-5 mb-5"><!-- margin-top: 5 margin-bottom: 5 -->
-    <div class="container"><!-- bootstrap style -->
-        <div class="row"><!-- bootstrap style -->
-            <h1 class="col-12 mx-auto mb-5 centered h1">Featured Trails</h1><!--margin-left/right: auto margin-bottom: 3rem (bootstrap)-->
-            <div class="col-12 mt-1"><!-- columns 12 (full width) margin-top: 1%-->
-                <div id="trails-vh">
-                    <div id="trails-container">
-                        <div class="container">
-                            <div class="row">
-                        <?php
-                          foreach($trails as $key => $trail){
-                        ?>
-                                <div class="col-6">
-                                    <a class="location d-block" target="_default" href="<?php echo $trail['url'] ?>"><!-- open in new tab -->
-                                      <div class='trail-img' style='background-image: url("<?php echo $trail['image'] ?>");'>
-                                        <h1 class='trail-header'><?php echo $key ?></h1>
-                                      </div>
-                                    </a>
-                                </div>
-                        <?php
-                          }
-                        ?>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
+<h1>Featured Trails</h1>
+<div id="trails-vh">
+  <div id="trails-container">
+    <?php
+      foreach($trails as $key => $trail){
+    ?>
+      
+      <a href="<?php echo $trail['url'] ?>">
+        <div class='trail-section' style='background-image: url("<?php echo $trail['image'] ?>");'>
+          <h1 class='trail-header'>
+              <?php echo $key ?>
+          </h1>
         </div>
-    </div>
-</section>
+      </a>
+    <?php
+      }
+    ?>
+  </div>
+</div>
