1335cbda951cf25e5c16ea3b10e8b2d941aacbdd
Guanzorumu <116049708+Guanzorumu@users.noreply.github.com>
2023-04-29 10:01:21 -0500

    Update join.php

--- pages/join.php
+++ pages/join.php
@@ -4,6 +4,8 @@ include('../config.php');//get DB connection
 require_once(__DIR__.'/../classes/Database.php');//mostly for DB connections... mostly...
 //initialize classes
 if(!isset($database)){$database = new Database();}//initialize database - why not....
+
+
 ?>
 <style>
     /**
@@ -46,26 +48,27 @@ if(!isset($database)){$database = new Database();}//initialize database - why no
                 <p class ="display-8 text-black text-center">
                     Wondering how to join on our next adventure? All you have to do is fill out the information below and we'll get back to you as soon as we can! Think we're taking awhile or have any questions? Find us on the Contact Page!
                 </p>
+                <div id="result" class="mt-2 mb-2 text-black"></div>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-5">
                     <form class="form form-floating callback-form form">
                         <div class="form-floating mb-4 mt-4">
-                            <input type="text" class="form-control" id ="name" placeholder="Name" autocomplete ="name" >
-                            <label for="name" class="text-black label-top">Name</label>
+                            <input type="text" class="form-control" id ="name" placeholder="Name" autocomplete ="name" required >
+                            <label for="name" class="text-black label-top">Name (Required)</label>
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="tel" class="form-control" id ="phone" placeholder="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" autocomplete="tel">
-                            <label for="tel" class="text-black label-top">Phone #</label>
+                            <input type="phone" class="form-control" id ="phone" placeholder="phone" pattern="[0-9]{3}[0-9]{3}[0-9]{4}" autocomplete="phone" required>
+                            <label for="phone" class="text-black label-top">Phone #(Required)</label>
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="email" class="form-control" id ="email" placeholder="Email Address" autocomplete="email">
-                            <label for="email" class="text-black label-top">Email</label>
+                            <input type="email" class="form-control" id ="email" placeholder="Email Address" autocomplete="email" required>
+                            <label for="email" class="text-black label-top">Email (Required)</label>
                         </div>
                         <div class="form-floating mb-4">
-                            <input type="text" class="form-control" id ="socialmediainfo" placeholder="socialmediainfo" autocomplete="socialmediainfo">
-                            <label for="socialmediainfo" class="text-black label-top">Socials</label>
+                            <input type="text"  class="form-control" id ="social" placeholder="social" autocomplete="social">
+                            <label for="social" class="text-black label-top">Socials</label>
                         </div>
                         <div class="form-floating mb-4">
                             <input type="text" class="form-control" id ="experience" pattern="[0-9]{2}" placeholder="experience" autocomplete="experience">
@@ -77,22 +80,21 @@ if(!isset($database)){$database = new Database();}//initialize database - why no
                             </p>
                         </div>
                         <div>
-                            <input type="radio" name="yes" id ="yesCheck" >
+                            <input type="radio" id ="rad" value="yes" >
                             <label for="yesCheck" class="text-black">Yes</label>
                         </div>
                         <div>
-                            <input type="radio" name="no" id ="noCheck" >
+                            <input type="radio" id ="rad" value="no" >
                             <label for="noCheck" class="text-black">No</label>
                         </div>
                         <div class="form-floating mb-4">
-                            <textarea type="textarea" class="form-control" id ="experience" rows="4" cols="40" placeholder="experience" autocomplete="experience"></textarea>
-                            <label for="experience" class="text-black label-top">Additional Info</label>
+                            <textarea type="textarea" class="form-control" id ="addinfo" rows="4" cols="40" placeholder="addinfo" autocomplete="addinfo"></textarea>
+                            <label for="addinfo" class="text-black label-top">Additional Info</label>
                         </div>
                         <div class="form-floating mb-4">
                             <button id="joinBtn" class="btn btn-outline text-black">Submit</button>
                         </div>
                     </form>
-                    
                 </div>
             </div>
         </div>
