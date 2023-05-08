f46b2c1e3fe1ac3caaaa65f47f4e4c30d96b910f
Guanzorumu <116049708+Guanzorumu@users.noreply.github.com>
2023-05-08 10:36:20 -0500

    Update join.php

--- workers/join.php
+++ workers/join.php
@@ -18,39 +18,43 @@ if( isset($_POST) ){
     } 
 
 
-        $username = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
         $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
         $info = filter_var($_POST['socialmediainfo'], FILTER_SANITIZE_STRING);
         $phone = filter_var($_POST['phone'], FILTER_SANITIZE_STRING);
         $yearsHiking = filter_var($_POST['experience'], FILTER_SANITIZE_STRING);
         $hasBeenGuide = filter_var($_POST['yesCheck'], FILTER_SANITIZE_STRING);
         
-         if($database->setApplication($username, $email, $info, $phone, $yearsHiking, $hasBeenGuide))
+         if($database->setApplication($name, $email, $info, $phone, $yearsHiking, $hasBeenGuide))
         {
             die(print(json_encode(array('error'=> 0, 'msg' => "Successfully submitted your join request!",'data'=>$database->getApplication()))));
         }else{
             die(print(json_encode(array('error'=> 1, 'msg' => $database->printError() ))));
         }
-        /**
-         * 
-         * example send email
-    $subject = "Alabama Hiing trails contact form ";
+        
+         
+    
+    $subject = "Alabama Hiking trails contact form for $name";
 
     //email body
-    $message_body = "Alabama Hiing trails join form \nname: $name";
+    $message_body = "Alabama Hiking trails join form \nname: $name";
     $message_body .= "\r\nEmail: $email\r\nmessage: $message";
 
     // In case any of our lines are larger than 70 characters
     $message_final = wordwrap($message_body, 70, "\r\n");
 
-    // compose headers
+     //compose headers
     $headers = "From: ".$email."\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "X-Mailer: PHP/".phpversion();
+    
+    //mail('maplestudios4@gmail.com', $subject, $message_final, $headers);
+    $send_mail = mail('UnivHikingTrails@gmail.com', $subject, $message_final, $headers);
 
-    $send_mail = mail('UnivHikingUSM@gmail.com', $subject, $message_final, $headers);
-
-    if($send_mail)
-         */
+    if($send_mail){
+        die(print(json_encode(array('error'=> 0, 'msg' => "<p class='alert alert-primary'>Thank you, $name! Your message has been sent.</p>"))));
+    }else{
+        die(print(json_encode(array('error'=> 1, 'msg' => "Error sending contact form."))));
+    }
 }
 ?>
